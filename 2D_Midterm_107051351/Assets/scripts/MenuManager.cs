4ba00e0686554ddf2b35177b4986240dbfadebe1
BoGao123 <71680028+BoGao123@users.noreply.github.com>
2020-11-20 10:11:46 +0800

    期中

---
+++ 2D_Midterm_107051351/Assets/scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+
+    {
+
+        print("開始遊戲");
+
+        SceneManager.LoadScene("關卡");
+
+    }
+
+    public void QuitGame()
+
+    {
+
+        print("離開遊戲");
+
+        Application.Quit();
+    }   
+}
